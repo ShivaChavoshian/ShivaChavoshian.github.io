7a43ab257f4a0c6c4687610b6c899ead1d8847a9
Saeed <saeed.uni68@gmail.com>
2022-01-09 15:04:53 +0100

    Upgrade website

---
+++ run.sh
@@ -0,0 +1,20 @@
+
+for image in */*jpg
+do
+ if [ -s $image ] ; then   # non-zero file size
+		jhead -cl "Chavoshian.com - Tazhib Traditional Artworks" $image;
+		exiftool -XMP-dc:Creator="Shiva Chavoshian" $image;
+		exiftool -overwrite_original -rights="©2022 Shiva Chavoshian, all rights reserved" -CopyrightNotice="©2022 Shiva Chavoshian, all rights reserved" $image;
+		rm "${image}_original";
+    echo "ExifData $image Updated.";
+  fi
+done
+
+
+
+
+
+# width=$(identify -format %w $image)
+# convert -background '#0008' -fill white -gravity center \
+# -size ${width}x30 caption:my_caption \
+# $image +swap -gravity south -composite new-$image
