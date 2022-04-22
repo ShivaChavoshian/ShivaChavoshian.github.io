aec7b5643fd73c974a449c3bbc9538d609c460b2
Saeed <saeed.uni68@gmail.com>
2022-04-22 21:45:36 +0200

    Update img optimizer

--- run.sh
+++ run.sh
@@ -1,19 +1,34 @@
 
 for image in */*jpg
 do
- if [ -s $image ] ; then   # non-zero file size
+	if [ -s $image ] ; then # non-zero file size
+
+		echo "*** Otimizing $image - File size: $(( $(wc -c < "$image") / 1000 ))";
+
+		convert $image -sampling-factor 4:2:0 -strip -quality 80 -interlace JPEG -colorspace sRGB $image;
+
+		echo "*** File size optimized: $(( $(wc -c < "$image") / 1000 ))";
+		echo "*** Updating image's meta data";
+
 		jhead -cl "Chavoshian.com - Tazhib Traditional Artworks" $image;
-		exiftool -XMP-dc:Creator="Shiva Chavoshian" $image;
-		exiftool -overwrite_original -rights="©2022 Shiva Chavoshian, all rights reserved (Chavoshian.com)" -CopyrightNotice="©2022 Shiva Chavoshian, all rights reserved (Chavoshian.com)" $image;
+		exiftool -XMP-dc:Creator="Shiva Chavoshian (Chavoshian.com)" $image;
+		exiftool -overwrite_original -rights="©2022 Shiva Chavoshian, all rights reserved." -CopyrightNotice="©2022 Shiva Chavoshian, all rights reserved." $image;
 		rm "${image}_original";
-    echo "ExifData $image Updated.";
-  fi
+
+		echo -e "=================================================\n";
+
+ 	fi
 done
 
 
 
 
+# More: https://developers.google.com/speed/docs/insights/OptimizeImages
+# Mpre: https://stackoverflow.com/questions/53777917/download-optimized-images-from-pagespeed-insights
+# convert INPUT.jpg -sampling-factor 4:2:0 -strip [-resize WxH] [-quality N] [-interlace JPEG] [-colorspace Gray/sRGB] OUTPUT.jpg
 
+
+# DIMENSION=$(identify -format '%wx%h' "$image");
 # width=$(identify -format %w $image)
 # convert -background '#0008' -fill white -gravity center \
 # -size ${width}x30 caption:my_caption \
